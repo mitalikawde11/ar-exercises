0c18178385e99f9ef406a28a417dfa1876d0b93a
Mitali <mitali.kawde11@gmail.com>
2023-08-24 23:02:40 +0530

    calculations using AR's .sum & average methods

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,15 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Calculations
+# the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue for the entire company: ", total_revenue
+
+# the average annual revenue for all stores.
+avg_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue for all stores: ", avg_revenue
+
+# the number of stores that are generating $1M or more in annual sales.
+stores_count = Store.where(annual_revenue: 1000000..).count
+puts "the number of stores that are generating $1M or more in annual sales: ", stores_count
