9e3bd20a9bf66ed62066f95b6799ba34dc57d27e
Mitali <mitali.kawde11@gmail.com>
2023-08-24 23:24:06 +0530

    adding data into employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,8 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# One-to-many association
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Priyanka", last_name: "Patel", hourly_rate: 35)
+@store2.employees.create(first_name: "Aaditya", last_name: "Mishra", hourly_rate: 50)
+@store2.employees.create(first_name: "Sonam", last_name: "Tata", hourly_rate: 65)
